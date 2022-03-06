5fa5bf62ca97934e4714ec3d14a606845249541e
sandeep194920 <sandeepamaranath@gmail.com>
2022-03-05 22:46:47 -0500

    fixed route

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(cors())
 app.use(xss())
 
 // routes
+app.get('/',(req,res)=>res.send('Jobs API Working'))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
